f05ec2f97bb204ea34f9caebc07c333f807bee67
Ben W <bwiedeback@gmail.com>
2020-10-12 09:31:03 -0600

    initialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
